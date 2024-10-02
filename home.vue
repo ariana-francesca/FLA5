035acb40f16ac6e71a1e7b7b6f103510f45f7532
Ariana <anpunsalang@student.hau.edu.ph>
2024-10-02 19:04:06 +0800

    Create home.vue

---
+++ home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="home">
+    <img src="@/assets/bini-banner.jpg" alt="BINI" class="bini" />
+    <h1>{{ msg }}</h1>
+    
+    <section class="introduction">
+      <p>BINI is a Filipino girl group formed under Star Hunt Academy, known for their catchy songs and captivating performances in the P-pop scene.</p>
+    </section>
+
+    <section class="follow-us">
+      <h2>Follow them on their Social Media Accounts</h2>
+        <a href="https://www.instagram.com/bini_ph">Instagram</a>
+        <a href="https://twitter.com/bini_ph">Twitter</a>
+        <a href="https://www.facebook.com/BINIph">Facebook</a>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to BINI FanPage!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+  padding: 20px;
+  color: aliceblue;
+}
+
+
+h1 {
+  color: black;
+  padding-top: 1cm;
+}
+
+.follow-us {
+  color: black;
+}
+
+p {
+  font-size: .6cm;
+}
+</style>
